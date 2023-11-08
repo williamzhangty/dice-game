60299efde31053967b7e8da9f25fe4de2057ee19
William Zhang <williamzhangty@gmail.com>
2023-11-08 13:30:08 -0600

    firt version of Dice Game

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,15 @@
 'use strict';
 
 const content = document.getElementById('content');
-const error = document.getElementById('error-name');
+const content1 = document.getElementById('content1');
 const result = document.getElementById('result');
 const counter = document.getElementById('counter');
-
 const playAgain = document.getElementById("play-again");
-
 const restart = document.getElementById("restart");
-
-        // Add a click event listener to the button
 
 let count = 1;
 let sum = 0;
+
 restart.addEventListener("click", function() {
     // Reload the page when the button is clicked
     location.reload();
@@ -24,43 +21,53 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 playAgain.addEventListener("click", function() {
     if (count >= 3) {
         playAgain.setAttribute("disabled", "true");
         playAgain.style.backgroundColor = '#d8d8d8';
+        restart.style.display = 'block';
+        playAgain.style.display = 'none';
     }
-   // alert(1);
-    //let i = 6;
-    //num = i % 3
-    ///content.innerHTML = num;
+   
     counter.innerHTML = 'Round:' + count;
     let num = getRandomInt(1, 6);
-    content.innerHTML = num;
+
+    //show different dice pic according to the random number
+    if (num == 1) {
+        
+        content1.style.background = "rgb(0 0 0 / 0.1) url('./assets/img/1.png') no-repeat center";
+        
+    } else  if (num == 2) {
+        
+        content1.style.background = "rgb(0 0 0 / 0.1) url('./assets/img/2.png') no-repeat center";
+        
+    } else  if (num == 3) {
+        
+        content1.style.background = "rgb(0 0 0 / 0.1) url('./assets/img/3.png') no-repeat center";
+        
+    } else  if (num == 4) {
+        
+        content1.style.background = "rgb(0 0 0 / 0.1) url('./assets/img/4.png') no-repeat center";
+        
+    } else  if (num == 5) {
+        
+        content1.style.background = "rgb(0 0 0 / 0.1) url('./assets/img/5.png') no-repeat center";
+        
+    } else  if (num == 6) {
+        
+        content1.style.background = "rgb(0 0 0 / 0.1) url('./assets/img/6.png') no-repeat center";
+    } 
+
     sum = sum + num;
     if (count == 1) {
         result.innerHTML = num
     } else if (count == 3) {
-        result.innerHTML = 'Your total points is : ' + result.innerHTML+'+'+num + '=' + sum;
+        result.innerHTML = 'Your total points are : ' + result.innerHTML+'+'+num + '=' + sum;
     } else {
         result.innerHTML = result.innerHTML+'+'+num ;
     }
     count ++;
     
-
 });
-
-
-
-function isNumber(str) {
-    let input = str.trim();
-
-    if (input.length > 0 && !isNaN(input)) {
-        return true;
-    }
-
-    return false;
-}
-
 
 
