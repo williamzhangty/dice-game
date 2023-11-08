40e82de0d672da8aa7c5d50c4acc9a0f30bd56cc
William Zhang <williamzhangty@gmail.com>
2023-11-07 22:21:17 -0600

    validate the input, compare the input and the random number, and generate the result

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const input = document.getElementById('input');
+const error = document.getElementById('error-name');
+const result = document.getElementById('result');
+
+function isNumber(str) {
+    let input = str.trim();
+
+    if (input.length > 0 && !isNaN(input)) {
+        return true;
+    }
+
+    return false;
+}
+
+let count = 0;
+
+input.addEventListener('keyup', function(){
+
+    let a = input.value;
+    let num = Math.floor(Math.random() * 10);
+
+    if (isNumber(a)) {
+        error.innerHTML = '';
+        if (a > num) {
+            //alert(num);
+            result.innerHTML = 'My number is bigger.';
+        } else {
+            //alert(num);
+
+            result.innerHTML = 'My number is smaller.';
+        }
+     
+        count = 0;
+    } else {
+        //alert(a);
+        error.innerHTML = 'Please input an invalid number';
+        
+    }
+    count++;
+//console.log(isNumber(a), isNumber(b));
+});
